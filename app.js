872809dc8c8e1209463c9175c27e70326a454e25
omarabbas32 <abbas8989omar@gmail.com>
2025-12-05 08:11:47 +0200

    Fix root route to serve index.html from root

--- app.js
+++ app.js
@@ -54,7 +54,7 @@ app.use(async (req, res, next) => {
 
 // Serve login page at root (before authentication)
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 // Auth routes (no authentication needed)
